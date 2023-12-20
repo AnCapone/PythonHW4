7c99bd7513bc7777212a75ae4ab1d7c78f1c6749
AnCapone <okai.kot44@gmail.com>
2023-12-20 20:51:56 +0200

    Add inline comments to First Task

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 inputString = str(input("Please, enter string"))
 digits = 0
 chars = 0
-for i in range(0, len(inputString)):
-    if inputString[i].isdigit():
-        digits += 1
-    elif inputString[i].isalpha():
-        chars += 1
+for i in inputString:  # цикл на обхід рядка посимвольно
+    if i.isdigit():
+        digits += 1  # якщо символ - число, додає одиницю до кількості цифр
+    elif i.isalpha():
+        chars += 1  # якщо символ - буква, додає одиницю до кількості букв
 
 print("Number of digits: ", digits)
 print("Number of chars: ", chars)
